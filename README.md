7b82e24075725e59306c4accca6683901369b495
Vladislav Tikhiy (SILENT) <brussian768@gmail.com>
2020-03-23 00:54:56 +0700

    Delete support for "years"

---
+++ README.md
@@ -0,0 +1,39 @@
+# string-milliseconds
+
+This function lets you convert a human-readable time string to milliseconds number.
+
+## Install
+
+`npm i git+https://github.com/tikhiy/string-milliseconds#v2.0.0`
+
+## Import
+
+```js
+// (TypeScript declaration file included as well)
+import ms from 'string-milliseconds';
+```
+
+## Example
+
+```js
+// syntax
+ms( '60 seconds' ); // -> 60000
+ms( '60 minutes' ); // -> 3600000
+ms( '24 hours' );   // -> 86400000
+ms( '30 days' );    // -> 2592000000
+ms( '12 months' );  // -> 31536000000
+
+// supports . (point)
+ms( '1.1s' );       // -> 1100
+ms( '1.1m' );       // -> 66000
+ms( '1.1h' );       // -> 3960000
+ms( '1.1d' );       // -> 95040000
+ms( '1.1 month' );  // -> 2890800000
+
+// supports [+-] sign
+ms( '-s' );         // -> -1000
+ms( '-m' );         // -> -60000
+ms( '-h' );         // -> -3600000
+ms( '-d' );         // -> -86400000
+ms( '-month' );     // -> -2628000000
+```
--- index.js
+++ index.js
@@ -2,32 +2,29 @@
 
 var toString = Object.prototype.toString;
 
+var regex = /^([+-]?(?:\d+|\d*\.\d+)?)\s?(months?|d(?:ays?)?|h(?:ours?)?|m(?:inutes?)?|s(?:econds?)?)$/;
+
 /**
- * @param {number|string} value
- * @returns {number}
+ * @param  {number | string} string
+ * @return {number}
+ *
  * @example
- * milliseconds( 'millisecond' );      // -> 1
- * milliseconds( '500 milliseconds' ); // -> 500
- * milliseconds( '10s' );              // -> 10000
- * milliseconds( 10 );                 // -> 10000
- * milliseconds( '-1 day' );           // -> -86400000
- * milliseconds( '2 years' );          // -> 63072000000
- * milliseconds( 'abc' );
- * // TypeError: cannot convert abc ([object String]) into milliseconds
+ * ms( '24 hours' ); // -> 86400000
  */
-function milliseconds ( value ) {
+function milliseconds ( string ) {
   var milliseconds;
 
-  if ( typeof value === 'number' ) {
-    milliseconds = value * 1000;
-  } else if ( typeof value === 'string' && /^\s*([+-]?(?:\d+|\d*\.\d+))?\s*(ms|s|m|h|d|y|milliseconds?|seconds?|minutes?|hours?|days?|years?)\s*$/.test( value ) ) {
-    milliseconds = RegExp.$1 ? + RegExp.$1 : 1;
+  if ( typeof string === 'number' ) {
+    milliseconds = string * 1000;
+  } else if ( typeof string === 'string' && regex.test( string ) ) {
+    milliseconds = RegExp.$1
+      ? Number( RegExp.$1 )
+      : 1;
 
     switch ( RegExp.$2 ) {
-      case 'years':
-      case 'year':
-      case 'y':
-        milliseconds *= 365;
+      case 'months':
+      case 'month':
+        milliseconds *= 365 / 12;
         /* falls through */
       case 'days':
       case 'day':
@@ -50,10 +47,10 @@ function milliseconds ( value ) {
         milliseconds *= 1000;
     }
   } else {
-    throw TypeError( 'cannot convert ' + value + ' (' + toString.call( value ) + ') into milliseconds' );
+    throw new Error( 'could not convert ' + string + ' (' + toString.call( string ) + ') into milliseconds' );
   }
 
-  return milliseconds || 0;
+  return Number(milliseconds.toFixed(2)) || 0;
 }
 
 module.exports = milliseconds;
